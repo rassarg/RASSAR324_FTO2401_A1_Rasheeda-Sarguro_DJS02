7e434748cb74c1a86f99c5b347227d4b63dad310
rasheeda <raskieda@gmail.com>
2024-05-08 13:37:03 +0200

    added try catch blocks

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+let isError = false;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -10,22 +11,40 @@ form.addEventListener("submit", (event) => {
 
   // Empty inputs validation
   if (dividend.trim() === "" || divider.trim() === "") {
-    result.classList.add("error-message");
     result.innerText =
       "Division not performed. Both values are required in inputs. Try again";
+    isError = true;
     // Dividing by 0 error
   } else if (divider == 0) {
-    result.classList.add("error-message");
-    result.innerText =
-      "Division not performed. Invalid number provided. Try again";
-    console.error("Error: Division by zero");
+    try {
+      throw new Error("Division by zero");
+    } catch (error) {
+      result.innerText =
+        "Division not performed. Invalid number provided. Try again.";
+      console.error(error);
+      isError = true;
+    }
     // NaN error
-  } else if (isNaN(dividend) || isNaN(divider)) {
-    result.classList.add("critical-error");
-    result.innerText = "Something critical went wrong. Please reload the page";
-    console.error("Error: Invalid inputs. Enter valid numbers.");
-    // Results displayed as whole number
   } else {
-    result.innerText = Math.floor(dividend / divider);
+    try {
+      if (isNaN(dividend) || isNaN(divider)) {
+        throw new Error("Invalid inputs. Enter valid numbers.");
+      } else {
+        result.innerText = Math.floor(dividend / divider);
+        isError = false; // False when input is valid
+      }
+    } catch (error) {
+      result.classList.add("critical-error");
+      result.innerText =
+        "Something critical went wrong. Please reload the page";
+      console.error(error);
+    }
+  }
+
+  // Add / remove error-message class styling for errors
+  if (isError) {
+    result.classList.add("error-message");
+  } else {
+    result.classList.remove("error-message");
   }
 });
