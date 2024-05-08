90125299c1cb1206a6edd8f9dd99287f30d2e895
rasheeda <raskieda@gmail.com>
2024-05-08 11:34:19 +0200

    added NaN error check

--- scripts.js
+++ scripts.js
@@ -6,17 +6,22 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  // Validation
+  // Validation & errors
 
-  // Empty inputs
+  // Empty inputs validation
   if (dividend.trim() === "" || divider.trim() === "") {
     result.innerText =
       "Division not performed. Both values are required in inputs. Try again";
-    // Dividing by 0
+    // Dividing by 0 error
   } else if (divider == 0) {
     result.innerText =
       "Division not performed. Invalid number provided. Try again";
     console.error("Error: Division by zero");
+    // NaN error
+  } else if (isNaN(dividend) || isNaN(divider)) {
+    result.classList.add("critical-error");
+    result.innerText = "Something critical went wrong. Please reload the page";
+    console.error("Error: Invalid inputs. Enter valid numbers.");
     // Results displayed as whole number
   } else {
     result.innerText = Math.floor(dividend / divider);
