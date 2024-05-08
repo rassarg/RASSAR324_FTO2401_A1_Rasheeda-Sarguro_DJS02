496f8e143332f15b305d15233ea67a3090dff3cf
rasheeda <raskieda@gmail.com>
2024-05-08 11:03:04 +0200

    validation for empty inputs added

--- scripts.js
+++ scripts.js
@@ -5,5 +5,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider); //Dividing numbers result in a decimal number
+
+  // Validation for empty inputs
+  if (dividend.trim() === "" || divider.trim() === "") {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
+  } else {
+    // Whole number result
+    result.innerText = Math.floor(dividend / divider);
+  }
 });
